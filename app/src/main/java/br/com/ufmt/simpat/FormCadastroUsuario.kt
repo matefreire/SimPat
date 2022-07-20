6343617806aeccb85d5fd13966cd8e6fd9f2abe9
matefreire <matefreire@github.com>
2022-07-20 12:26:52 -0400

    firebase cadastro 20/07

--- app/src/main/java/br/com/ufmt/simpat/FormCadastroUsuario.kt
+++ app/src/main/java/br/com/ufmt/simpat/FormCadastroUsuario.kt
@@ -12,4 +12,24 @@ class FormCadastroUsuario : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro_usuario)
     }
+
+    //    private fun createAccount(email: String, password: String) {
+//        // [START create_user_with_email]
+//        auth.createUserWithEmailAndPassword(email, password)
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    // Sign in success, update UI with the signed-in user's information
+//                    Log.d(TAG, "createUserWithEmail:success")
+//                    val user = auth.currentUser
+//                    updateUI(user)
+//                } else {
+//                    // If sign in fails, display a message to the user.
+//                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+//                    Toast.makeText(baseContext, "Authentication failed.",
+//                        Toast.LENGTH_SHORT).show()
+//                    updateUI(null)
+//                }
+//            }
+//        // [END create_user_with_email]
+//    }
 }
--- app/src/main/java/br/com/ufmt/simpat/FormLogin.kt
+++ app/src/main/java/br/com/ufmt/simpat/FormLogin.kt
@@ -16,7 +16,6 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import br.com.ufmt.simpat.FormCadastroUsuario as FormCadastroUsuario
 
-
 class FormLogin : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
@@ -31,22 +30,15 @@ class FormLogin : AppCompatActivity() {
             auth = Firebase.auth
 
         //Buscando informações da tela para validação e encaminhamento
-        val btnLogin:Button = findViewById(R.id.btn_login)
-        val editTxtEmail:EditText = findViewById(R.id.inputemail)
-        val editTxtSenha:EditText = findViewById(R.id.inputsenha)
+
         val txtSemConta:TextView = findViewById(R.id.text_sem_conta)
         val txtEsqueceuSenha:TextView = findViewById(R.id.text_esqueceu_senha)
 
+        val btnLogin:Button = findViewById(R.id.btn_login)
+
         //Criando intenção para o login
         btnLogin.setOnClickListener {
-            val intent = Intent(this@FormLogin, Home::class.java)
-
-            if (editTxtEmail.toString() == "" || editTxtSenha.toString() == "") {
-                Toast.makeText(applicationContext, "Os campos de login e senha são obrigatórios!", Toast.LENGTH_LONG).show()
-            //Criar Validações
-            } else {
-                startActivity(intent)
-            }
+            autentica()
         }
 
         //Criando intenção para o cadastro
@@ -66,31 +58,35 @@ class FormLogin : AppCompatActivity() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
-        if(currentUser != null){
+        if (currentUser != null) {
             reload();
         }
     }
 
-//    private fun createAccount(email: String, password: String) {
-//        // [START create_user_with_email]
-//        auth.createUserWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this) { task ->
-//                if (task.isSuccessful) {
-//                    // Sign in success, update UI with the signed-in user's information
-//                    Log.d(TAG, "createUserWithEmail:success")
-//                    val user = auth.currentUser
-//                    updateUI(user)
-//                } else {
-//                    // If sign in fails, display a message to the user.
-//                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-//                    Toast.makeText(baseContext, "Authentication failed.",
-//                        Toast.LENGTH_SHORT).show()
-//                    updateUI(null)
-//                }
-//            }
-//        // [END create_user_with_email]
-//    }
+    private fun sendEmailVerification() {
+        // [START send_email_verification]
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                // Email Verification sent
+            }
+        // [END send_email_verification]
+    }
 
+    private fun autentica(){
+
+        val editTxtEmail:EditText = findViewById(R.id.inputemail)
+        val editTxtSenha:EditText = findViewById(R.id.inputsenha)
+        val intent = Intent(this@FormLogin, Home::class.java)
+
+        if (editTxtEmail.toString() == "" || editTxtSenha.toString() == "") {
+            Toast.makeText(applicationContext, "Os campos de login e senha são obrigatórios!", Toast.LENGTH_LONG).show()
+            //Criar Validações
+        } else {
+
+            startActivity(intent)
+        }
+    }
 
     private fun reload() {
 
