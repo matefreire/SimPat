27e61a153f08ff32afc9588268f2158afc697469
matefreire <matefreire@github.com>
2022-07-20 02:03:31 -0400

    Integração Firebase

--- app/src/main/java/br/com/ufmt/simpat/FormCadastroUsuario.kt
+++ app/src/main/java/br/com/ufmt/simpat/FormCadastroUsuario.kt
@@ -2,10 +2,23 @@ package br.com.ufmt.simpat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class FormCadastroUsuario : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro_usuario)
     }
+
+//    public override fun onStart() {
+//        super.onStart()
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser = auth.currentUser
+//        if(currentUser != null){
+//            reload();
+//        }
+//    }
 }
--- app/src/main/java/br/com/ufmt/simpat/FormLogin.kt
+++ app/src/main/java/br/com/ufmt/simpat/FormLogin.kt
@@ -8,20 +8,30 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import br.com.ufmt.simpat.FormCadastroUsuario as FormCadastroUsuario
 
 
 class FormLogin : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_login)
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        //Firebase
+            // Initialize Firebase Auth
+        auth = Firebase.auth
+
         title = "SimPat"
 
         //Buscando informações da tela para validação e encaminhamento
         val btnLogin:Button = findViewById(R.id.btn_login)
-        val editTxtLogin:EditText = findViewById(R.id.inputlogin)
+        val editTxtEmail:EditText = findViewById(R.id.inputemail)
         val editTxtSenha:EditText = findViewById(R.id.inputsenha)
         val txtSemConta:TextView = findViewById(R.id.text_sem_conta)
         val txtEsqueceuSenha:TextView = findViewById(R.id.text_esqueceu_senha)
@@ -30,7 +40,7 @@ class FormLogin : AppCompatActivity() {
         btnLogin.setOnClickListener {
             val intent = Intent(this@FormLogin, Home::class.java)
 
-            if (editTxtLogin.toString() == "" || editTxtSenha.toString() == "") {
+            if (editTxtEmail.toString() == "" || editTxtSenha.toString() == "") {
                 Toast.makeText(applicationContext, "Os campos de login e senha são obrigatórios!", Toast.LENGTH_LONG).show()
             //Criar Validações
             } else {
