6299cd34bf533a59444bccd43e166433980c19d3
Luiz Henrique <luizhenrique.tk@hotmail.com>
2022-07-20 13:58:55 -0400

    Evnedo de navegação entre as telas de Lista bem, home e cadastro usuário para tela de login.

--- app/src/main/java/br/com/ufmt/simpat/FormCadastroUsuario.kt
+++ app/src/main/java/br/com/ufmt/simpat/FormCadastroUsuario.kt
@@ -1,7 +1,9 @@
 package br.com.ufmt.simpat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -11,5 +13,14 @@ class FormCadastroUsuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro_usuario)
+
+
+        val txtCadastroHome: TextView = findViewById(R.id.textView)
+
+        //Criando intenção para voltar para Home
+        txtCadastroHome.setOnClickListener {
+            val intent = Intent(this@FormCadastroUsuario, FormLogin::class.java)
+            startActivity(intent)
+        }
     }
 }
