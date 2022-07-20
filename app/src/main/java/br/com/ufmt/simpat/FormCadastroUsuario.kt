6c5907a9070ddcf9454be726778947c5a73f2578
matefreire <matefreire@github.com>
2022-07-20 02:30:40 -0400

    firebase cadastro

--- app/src/main/java/br/com/ufmt/simpat/FormCadastroUsuario.kt
+++ app/src/main/java/br/com/ufmt/simpat/FormCadastroUsuario.kt
@@ -3,22 +3,13 @@ package br.com.ufmt.simpat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FormCadastroUsuario : AppCompatActivity() {
-
-    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro_usuario)
     }
-
-//    public override fun onStart() {
-//        super.onStart()
-//        // Check if user is signed in (non-null) and update UI accordingly.
-//        val currentUser = auth.currentUser
-//        if(currentUser != null){
-//            reload();
-//        }
-//    }
 }
