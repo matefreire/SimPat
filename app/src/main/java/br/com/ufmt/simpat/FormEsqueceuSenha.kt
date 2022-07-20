a21986d44cc71b6d6c99bf376704defc26759c06
Luiz Tanaka <luizhenrique.tk@hotmail.com>
2022-07-20 02:15:33 -0400

    Alteração do botao voltar - tela de login

--- app/src/main/java/br/com/ufmt/simpat/FormEsqueceuSenha.kt
+++ app/src/main/java/br/com/ufmt/simpat/FormEsqueceuSenha.kt
@@ -1,11 +1,22 @@
 package br.com.ufmt.simpat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class FormEsqueceuSenha : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_esqueceu_senha)
+
+
+        val txtEsqueceuSenhaVoltarHome: TextView = findViewById(R.id.textView)
+
+        //Criando intenção para voltar no menu principal
+        txtEsqueceuSenhaVoltarHome.setOnClickListener {
+            val intent = Intent(this@FormEsqueceuSenha, FormLogin::class.java)
+            startActivity(intent)
+        }
     }
 }
