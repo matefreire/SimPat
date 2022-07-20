b7f00c94d9d6f58c0f631c0df2d002be3d30804e
matefreire <matefreire@github.com>
2022-07-20 00:34:37 -0400

    Final do front, próximas alterações no front somente após implementarmos a lista

---
+++ app/src/main/java/br/com/ufmt/simpat/CadastrarBem.kt
@@ -0,0 +1,11 @@
+package br.com.ufmt.simpat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastrarBem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastrar_bem)
+    }
+}
---
+++ app/src/main/java/br/com/ufmt/simpat/ListaBem.kt
@@ -0,0 +1,11 @@
+package br.com.ufmt.simpat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaBem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_bem)
+    }
+}
